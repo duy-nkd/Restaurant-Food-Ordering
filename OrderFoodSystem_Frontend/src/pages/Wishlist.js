03515bac26d9cf6b51050022e0413efecbb2829b
duy-nkd <duy.nguyenkhanh.nkd@gmail.com>
2026-03-01 15:14:10 +0700

    fix img

--- OrderFoodSystem_Frontend/src/pages/Wishlist.js
+++ OrderFoodSystem_Frontend/src/pages/Wishlist.js
@@ -148,7 +148,7 @@ export default function Wishlist() {
                             src={item.product.imageUrl}
                             alt={item.product.nameProduct}
                             className="w-16 h-16 object-cover rounded shadow-sm"
-                            onError={(e) => { e.target.src = 'https://via.placeholder.com/100?text=No+Image'; }}
+                            onError={(e) => { e.target.src = 'https://via.placeholder.co/100?text=No+Image'; }}
                           />
                         )}
                         <span>{item.product?.nameProduct}</span>
